dbaf0c3a9368b78d510c7f405a86c058f7079de2
Mohit Thatte <mohit.thatte@gmail.com>
2011-06-18 16:50:57 +0530

    finished dice project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  
+  def initialize
+    @values = []
+  end
+  
+  def roll(num)
+    @values = []
+    num.times { values << rand(6) + 1}
+  end
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
@@ -48,6 +57,7 @@ class AboutDiceProject < EdgeCase::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this.
+    # A: --- test the object ids of the returned values ---
   end
 
   def test_you_can_roll_different_numbers_of_dice
